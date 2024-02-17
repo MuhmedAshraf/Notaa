3758a897ef09e92e1ddd4b0872a73a16fdd27d43
MuhmedAshraf <100963016+MuhmedAshraf@users.noreply.github.com>
2024-02-17 19:11:47 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         'signup': (context) => SignUp(),
         'home': (context) => Home(),
         'addNote': (context) => AddNote(),
-        //'addNote': (context) => AddNote(),
+
       },
       theme: ThemeData(
           primaryColor: Colors.blue,
